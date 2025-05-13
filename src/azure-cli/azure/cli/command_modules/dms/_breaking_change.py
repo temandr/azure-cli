496cc64d52b7fc60c26a528f085a2adf13b8af43
temandr <brentwoodpav@gmail.com>
2025-05-13 23:24:39 +0000

    Adding breaking changes file

---
+++ src/azure-cli/azure/cli/command_modules/dms/_breaking_change.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core.breaking_change import register_argument_deprecate
+
+register_argument_deprecate('dms create', '--location', target_version='2.70.0')
+register_argument_deprecate('dms project create', '--location', target_version='2.70.0')
